0509c485e9342bbf4f4fb7c7e9539911297fd654
Have Samuel <samiehave9@gmail.com>
2022-11-18 16:03:19 +0300

    Selecting Multipe elemnents in the DOM

---
+++ app.js
@@ -0,0 +1,16 @@
+// const allItemElements = document.querySelectorAll('li');
+const allItemElements = document.getElementsByTagName('li');
+
+for (const alEl of allItemElements) {
+  console.dir(alEl);
+}
+
+
+const h1 = document.getElementById('main-title');
+h1.textContent = 'Some other text!';
+h1.style.color = 'brown';
+h1.style.backgroundColor = 'pink';
+
+const li = document.querySelector('li:last-of-type');
+
+li.textContent = li.textContent + ' (Change)';
