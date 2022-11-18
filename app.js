b310fb3842a02bf13092d7f86d533e7b2d17591b
Have Samuel <samiehave9@gmail.com>
2022-11-18 20:42:40 +0300

    Styling the DOM elements

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const ul = document.body.firstElementChild.nextElementSibling;
 const fstLi = ul.firstElementChild;
 
 console.log(fstLi);
+
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+// section.style.backgroundColor = 'brown';
+
+button.addEventListener('click', () => {
+  // if (section.className === 'color visible') {
+  //   section.className = 'color invisible';
+  // } else {
+  //   section.className = 'color visible';
+  // }
+  section.classList.toggle('invisible');
+});
